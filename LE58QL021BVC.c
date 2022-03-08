45f585c4c2fd7d7efa5b6e7d5c60eed98d723e21
Renan Rodolfo da Silva <31565700+renaner123@users.noreply.github.com>
2022-03-08 09:06:44 -0300

    Update LE58QL021BVC.c

--- LE58QL021BVC.c
+++ LE58QL021BVC.c
@@ -1,1 +1,68 @@
 
+#include "LE58QL021BVC.h"
+
+Codec ::Codec () {
+
+}
+
+
+void Codec::Periodico(){ 
+   
+}
+
+
+void Codec::tirarGancho() {
+ 
+}
+
+
+void Codec::colocarGancho() { 
+
+}
+
+void Codec::verificarStatus(){ 
+   
+}
+
+
+void Codec::tocarRing() {
+ 
+}
+
+
+void Codec::verificarAtendimento() { 
+
+}
+
+
+void Codec::tomDiscagem(){ 
+   
+}
+
+
+void Codec::atendimento() {
+ 
+}
+
+
+void Codec::colocarGancho() { 
+
+}
+
+    void respostaPeriodico();
+  
+    void respostaTirarGancho(alt_u8 comando);
+  
+    void respostaColocarGancho(alt_u8 comando);
+  
+    void respostaVerificarStatus(alt_u8 comando);
+  
+    void respostaTocarRing(alt_u8 comando);
+  
+    void respostaVerificarAtendimento(alt_u8 comando);
+  
+    void respostaTomDiscagem(alt_u8 comando);
+  
+    void respostaAtendimento(alt_u8 comando);
+  
+    void respostaColocarGancho(alt_u8 comando); 
