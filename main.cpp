079befbc07ac84c97454d49f58f63cd5f4a2b37d
Renan <renaner123@gmail.com>
2022-04-16 15:44:57 -0300

    Metodos para iniciar codec

--- main.cpp
+++ main.cpp
@@ -16,42 +16,13 @@ int main(void) {
 	Placaramal teste;
 
 	teste.init();
-	usleep(1000000);
-
-	teste.tirarGancho(1);
-	usleep(1000000);
-
-	teste.tomDiscagem();
-	usleep(1000000);
-
-	teste.tocarRing();
-	usleep(1000000);
-
-	teste.atendimento();
-
-
-
-    //printf("%02x\n",tx_buf[0]);
-
+/* 	usleep(1000000);
 
 	// while (1) {
 
 
-	// 	printf("Escrevendo no CODEC 4 bytes: \n");
-	// 	IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
-	// 	alt_avalon_spi_command(SPI_MASTER_BASE, 0, 4, &tx_buf[0], 0, &rx_buf[0], 0);
-
-	// 	// Coloca um valor no buffer de saida do slave para ser enviado ao master.
-	// 	IOWR_ALTERA_AVALON_SPI_TXDATA(SPI_SLAVE_BASE, 0xAA);
-
-	// 	printf("Lendo do CODEC 4 bytes: \n");
-	// 	IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 0);
-	// 	alt_avalon_spi_command(SPI_MASTER_BASE, 0, 0, &tx_buf[0], 4, &rx_buf[0], 0);
-
-	// 	usleep(1000000);
-
 	// } // end while
 
 	return 0;
-
+*/
 } // end main
--- placaramal.cpp
+++ placaramal.cpp
@@ -11,21 +11,26 @@ int alt_avalon_spi_command(alt_u32 base, alt_u32 slave,
 */
 
 void Placaramal::init(){ 
+    
     this->tx_buf[0] = 0x4A;
     this->tx_buf[1] = 0x55;
     this->tx_buf[2] = 0x4A;
     this->tx_buf[3] = 0x54;
     this->tx_buf[4] = 0x4A;          
     this->tx_buf[5] = 0x53;
-    this->tx_buf[6] = 0x52;    
+    this->tx_buf[6] = 0x52;  
 
-    IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
+    //printf("%02x\n",tx_buf[0]);
+
+
+
+/*     IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
     alt_avalon_spi_command(SPI_MASTER_BASE, 0, TAMANHOBUFFER, this->tx_buf, 0, this->rx_buf, 0);
     
-    this->limparbuffer();
+    this->limparbuffer(); */
 }
 
-void Placaramal::periodico(){ 
+/* void Placaramal::periodico(){ 
     this->tx_buf[0] = PERIODICO;
 
     IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
@@ -33,110 +38,13 @@ void Placaramal::periodico(){
     
     this->limparbuffer();
    
-}
-
-void Placaramal::tirarGancho(int canal) {
-
-    this->tx_buf[1] = INTERRUPCAO;
-
-    if(canal==1){        
-        this->tx_buf[1] = ENDERECOCANAL1;
-    }else if (canal==2)
-    {
-        this->tx_buf[1] = ENDERECOCANAL2;
-    }else if(canal ==3){
-        this->tx_buf[1] = ENDERECOCANAL3;     
-    }else if(canal==4){
-        this->tx_buf[1] = ENDERECOCANAL3;       
-    }    
-
-    this->tx_buf[2] = LENDOESCREVENDO;        
-
-    IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
-    alt_avalon_spi_command(SPI_MASTER_BASE, 0, TAMANHOBUFFER, this->tx_buf, 0, this->rx_buf, 0);
-    
-    this->limparbuffer();
-   
-}
-
-
-void Placaramal::colocarGancho() { 
-    this->tx_buf[0] = INTERRUPCAO;
-    this->tx_buf[1] = COMANDOREPOUSO;
-
-    IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
-    alt_avalon_spi_command(SPI_MASTER_BASE, 0, TAMANHOBUFFER, this->tx_buf, 0, this->rx_buf, 0);
-    
-    this->limparbuffer(); 
-
-}
-
-void Placaramal::tocarRing() {
-    //Canal 1 ligando pro canal 2
-    this->tx_buf[0] = ATENDENDO;
-
-    IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
-    alt_avalon_spi_command(SPI_MASTER_BASE, 0, TAMANHOBUFFER, this->tx_buf, 0, this->rx_buf, 0);
-    
-    this->limparbuffer();  
-}
-
-
-void Placaramal::verificarAtendimento() {
-    //Verificar se recebeu 0x42
-
-    }
-
-
-void Placaramal::tomDiscagem(){ 
-    //Canal 1 ligando pro canal 2
-    this->tx_buf[0] = PERIODICO;
-    this->tx_buf[1] = CHAMANDO;
-
-    IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
-    alt_avalon_spi_command(SPI_MASTER_BASE, 0, TAMANHOBUFFER, this->tx_buf, 0, this->rx_buf, 0);
-    
-    this->limparbuffer();   
-}
-
-
-void Placaramal::atendimento() {
-     //Canal 1 ligando pro canal 2
-    this->tx_buf[0] = INTERRUPCAO;
-    this->tx_buf[1] = ATENDENDO;
-
-    IOWR_ALTERA_AVALON_PIO_DATA(TX_EN_BASE, 1);
-    alt_avalon_spi_command(SPI_MASTER_BASE, 0, TAMANHOBUFFER, this->tx_buf, 0, this->rx_buf, 0);
-    
-    this->limparbuffer();   
 }
 
 void Placaramal::limparbuffer(){
     for(int i=0;i<TAMANHOBUFFER;i++){
         this->tx_buf[i] = {};
         this->rx_buf[i] = {};
-    }
-  
+    }  
 }
-
-Slave::Slave () {
-
-}
-
- void Slave::respostaPeriodico(){};
-
- void Slave::respostaTirarGancho(alt_u8 comando){};
-
- void Slave::respostaColocarGancho(alt_u8 comando){};
-
- void Slave::respostaVerificarStatus(alt_u8 comando){};
-
- void Slave::respostaTocarRing(alt_u8 comando){};
-
- void Slave::respostaVerificarAtendimento(alt_u8 comando){};
-
- void Slave::respostaTomDiscagem(alt_u8 comando){};
-
- void Slave::respostaAtendimento(alt_u8 comando){};
 
 
